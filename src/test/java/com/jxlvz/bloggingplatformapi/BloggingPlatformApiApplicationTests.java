7bfab36657657999e8bdc6deb8a0dc568292751a
jxlvz (José Álvarez) <jalvchi9@gmail.com>
2025-09-22 22:30:36 +0200

    feat: initial project structure

---
+++ src/test/java/com/jxlvz/bloggingplatformapi/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxlvz.bloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
