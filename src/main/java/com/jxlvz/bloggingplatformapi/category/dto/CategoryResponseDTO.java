6cb5741cd114560dc9c7c9ec453e29ceab944899
jxlvz (José Álvarez) <jalvchi9@gmail.com>
2025-10-06 13:03:50 +0200

    feat(category): add CategoryResponseDTO

---
+++ src/main/java/com/jxlvz/bloggingplatformapi/category/dto/CategoryResponseDTO.java
@@ -0,0 +1,11 @@
+package com.jxlvz.bloggingplatformapi.category.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryResponseDTO {
+  private Long id;
+  private String name;
+}
